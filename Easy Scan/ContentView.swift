c951e45147fde96e9df7a68d84e09db4f5f63da8
zhihaofans <zhihaofans@hotmail.com>
2024-11-23 21:35:58 +0800

    Initial Commit

---
+++ Easy Scan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Easy Scan
+//
+//  Created by zzh on 2024/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Easy Scan/Easy_ScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Easy_ScanApp.swift
+//  Easy Scan
+//
+//  Created by zzh on 2024/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Easy_ScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
